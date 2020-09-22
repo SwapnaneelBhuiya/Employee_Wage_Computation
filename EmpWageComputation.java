d43928b61c59edb5a1b99bc3e1355128cdff83d0
swapnaneel bhuiya <bhuiya.swapnaneel@gmail.com>
2020-09-22 15:16:57 +0530

    [swapnaneel]UC4: Solve using switch case

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,14 +3,29 @@ public class EmpWageComputation
     public static void main(String args[])
     {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-        int empCheck= (int)(Math.random()*10)/5;
+        int empCheck= (int)(Math.random()*10)%2;
         if(empCheck==1)
+        {
             System.out.println("Employee Present");
+            int hours_day=8, wage_hr=20;
+            int daily_wage=8*20;
+            int part_time_hour=4, part_time_wage=20;
+            int empType=(int) (Math.random()*10)%3;
+            switch(empType)
+            {
+                case 1:
+                    daily_wage=8*20;
+                    break;
+                case 2:
+                    daily_wage=4*20;
+                    break;
+                default:
+                    daily wage =0;
+            }
+        }
         else
             System.out.println("Employee Absent");
-        int hours_day=8, wage_hr=20;
-        int daily_wage=8*20;
-        int part_time_hour=8;
-        int empType=(int) (Math.random()*10)/5;
+
+
     }
 }
