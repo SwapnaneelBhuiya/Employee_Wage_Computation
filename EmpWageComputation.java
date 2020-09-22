47b1fa8f23678128bfddfc08bdcc5df659e428e8
swapnaneel bhuiya <bhuiya.swapnaneel@gmail.com>
2020-09-22 15:29:34 +0530

    [swapnaneel]UC6:Calculate Wages till a condition of total working hours or days is reached

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,8 +3,8 @@ public class EmpWageComputation
     public static void main(String args[])
     {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-        int i=0, wages_month=0;
-        for(i=0;i<20;i++) {
+        int i=0, wages_month=0, hours=0;
+        while(i<20&&hours<100) {
             int empCheck = (int) (Math.random() * 10) % 2;
             if (empCheck == 1) {
                 System.out.println("Employee Present");
@@ -15,17 +15,21 @@ public class EmpWageComputation
                 switch (empType) {
                     case 1:
                         daily_wage = 8 * 20;
+                        hours+=8;
                         break;
                     case 2:
                         daily_wage = 4 * 20;
+                        hours+=4;
                         break;
                     default:
                         daily_wage = 0;
                 }
                 wages_month=wages_month+daily_wage;
+                i++;
             }
             else
                 System.out.println("Employee Absent");
         }
+        System.out.println("Total wage ="+wages_month);
     }
 }
