436f119a19612fe2d21da9b07d64e75fb9b6d834
swapnaneel bhuiya <bhuiya.swapnaneel@gmail.com>
2020-09-22 15:23:17 +0530

    [swapnaneel]UC5: Calculate wages for a month

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,29 +3,29 @@ public class EmpWageComputation
     public static void main(String args[])
     {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-        int empCheck= (int)(Math.random()*10)%2;
-        if(empCheck==1)
-        {
-            System.out.println("Employee Present");
-            int hours_day=8, wage_hr=20;
-            int daily_wage=8*20;
-            int part_time_hour=4, part_time_wage=20;
-            int empType=(int) (Math.random()*10)%3;
-            switch(empType)
-            {
-                case 1:
-                    daily_wage=8*20;
-                    break;
-                case 2:
-                    daily_wage=4*20;
-                    break;
-                default:
-                    daily_wage =0;
+        int i=0, wages_month=0;
+        for(i=0;i<20;i++) {
+            int empCheck = (int) (Math.random() * 10) % 2;
+            if (empCheck == 1) {
+                System.out.println("Employee Present");
+                int hours_day = 8, wage_hr = 20;
+                int daily_wage = 8 * 20;
+                int part_time_hour = 4, part_time_wage = 20;
+                int empType = (int) (Math.random() * 10) % 3;
+                switch (empType) {
+                    case 1:
+                        daily_wage = 8 * 20;
+                        break;
+                    case 2:
+                        daily_wage = 4 * 20;
+                        break;
+                    default:
+                        daily_wage = 0;
+                }
+                wages_month=wages_month+daily_wage;
             }
+            else
+                System.out.println("Employee Absent");
         }
-        else
-            System.out.println("Employee Absent");
-
-
     }
 }
